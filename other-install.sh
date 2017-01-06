59def1e9b84f071a0091b394d7e607415d157615
zwf <1094646850@qq.com>
2017-01-06 22:31:51 +0800

    添加aria2下载工具的安装

---
+++ other-install.sh
@@ -0,0 +1,8 @@
+# 尝试着将tool工具打包压缩上传
+# ########################################################################################
+# 
+# 安装aria2下载器
+# wget https://github.com/aria2/aria2/releases/download/release-1.30.0/aria2-1.30.0.tar.gz
+# 解压进去后三步走
+# cp ./aria2.conf 到安装目录中
+# aria2c --conf-file=
